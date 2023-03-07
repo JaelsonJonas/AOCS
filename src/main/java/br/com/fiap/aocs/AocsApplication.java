fe2f9b766d02ed130d55976851d95f6e37dc8aaa
JaelsonJonas <rm93915@fiap.com.br>
2023-03-06 21:52:38 -0300

    incluindo o boiler plate do Spring

---
+++ src/main/java/br/com/fiap/aocs/AocsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.aocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/aocs/AocsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.aocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
