28459deb0be4dd78fd7734e3c129b3eb54a85530
JaelsonJonas <rm93915@fiap.com.br>
2023-03-14 20:25:27 -0300

    Incluindo metodo returnWithId() e o create()

--- src/main/java/br/com/fiap/aocs/controllers/TarefaController.java
+++ src/main/java/br/com/fiap/aocs/controllers/TarefaController.java
@@ -1,21 +1,51 @@
 package br.com.fiap.aocs.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.fiap.aocs.models.Tarefa;
 
 @RestController
 public class TarefaController {
 
-    @GetMapping("/api/tarefa")
-    public Tarefa retorna() {
+    private List<Tarefa> tarefas = new ArrayList<Tarefa>();
 
-        return new Tarefa(69,"Estudar Java", "Estudando JPA com Hibernate", LocalDate.parse("2023-03-13"),
-                LocalTime.parse("00:40"));
+    @GetMapping("/api/tarefa")
+    public List<Tarefa> listAll() {
+
+        return tarefas;
     }
 
+    @PostMapping("api/tarefa")
+    public ResponseEntity<Tarefa> create(@RequestBody Tarefa tarefa, UriComponentsBuilder uriCompBuilder) {
+        tarefa.setId(tarefas.size() + 1);
+        tarefas.add(tarefa);
+
+        URI uri = uriCompBuilder.path("api/tarefa/{id}").buildAndExpand(tarefa.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(tarefa);
+        // return ResponseEntity.status(HttpStatus.CREATED).body(tarefa);
+    }
+
+    @GetMapping("api/tarefa/{id}")
+    public ResponseEntity<Tarefa> returnWithId(@PathVariable Integer id) {
+
+        Optional<Tarefa> taferaConteiner = tarefas.stream().filter((Tarefa t) -> t.getId().equals(id)).findFirst();
+
+        if (taferaConteiner.isPresent())
+            return ResponseEntity.ok(taferaConteiner.get());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
 }
