fea2215b58b4ffef2c118e976268949571928558
JaelsonJonas <livedojonas@hotmail.com>
2023-08-28 04:01:29 -0300

    Config de segurança corrigido.

--- src/main/java/br/com/fiap/aocs/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/aocs/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -21,14 +21,15 @@ public class SecurityConfig {
     private AuthorizationFilter filter;
 
     @Autowired
-    private Environment env;
+    Environment env;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers(HttpMethod.POST, "api/register", "api/login").permitAll())
+                        .requestMatchers(new AntPathRequestMatcher("/api/register")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/login")).permitAll())
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
@@ -37,8 +38,7 @@ public class SecurityConfig {
             http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.anyRequest().authenticated());
         }
 
-        return http.build();
-    }
+        return http.build();    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
